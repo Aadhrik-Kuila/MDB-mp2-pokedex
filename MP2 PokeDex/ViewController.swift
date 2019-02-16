e5716847111ee87c4b8ef054364b00c38b3e0876
Aadhrik Kuila <aadhrik.kuila@berkeley.edu>
2019-02-15 17:21:22 -0800

    Initial Commit

---
+++ MP2 PokeDex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MP2 PokeDex
+//
+//  Created by Aadhrik Kuila on 2/15/19.
+//  Copyright © 2019 Aadhrik Kuila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
